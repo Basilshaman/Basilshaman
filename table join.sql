9a2f1e994ddeb0409c6f027b4d6ed95cfafbcf4b
Basilshaman <basilshaman179@gmail.com>
2025-03-06 16:52:24 +0530

    Add files via upload

--- table join.sql
+++ table join.sql
@@ -47,3 +47,52 @@ select * from Employees  e right join Departments d on e.DepartmentID=d.Departme
 select * from Employees  e left join Departments d on e.DepartmentID=d.DepartmentID
 union
 select * from Employees  e right join Departments d on e.DepartmentID=d.DepartmentID;
+delimiter //
+create procedure ed()
+begin
+	select * from Employees join Departments on Employees.DepartmentID=Departments.DepartmentID;
+end //
+delimiter ;
+
+call ed();
+
+create table student(
+id int  auto_increment,
+s_name varchar(30) not null,
+place varchar(30) not null,
+class int,
+s_status varchar(30) default "PASS",
+mark int,
+check (mark>=45),
+primary key(id));
+select * from student;
+select * from sdt;
+
+create table sdt(
+s_id int,
+namee varchar(30) not null,
+place varchar(30) not null,
+email varchar(50) unique,
+constraint fk_id foreign key (s_id) references student(id));
+insert into student (id,s_name,place,class,mark)values(
+50001,'dude','kottayam',9,47),
+(50002, 'denny','kollam',3,56),
+(50003,'dominik','kannur',8,45);
+insert into student(s_name,place,class,mark)values(
+'benny','malappuram',8,57),
+('dominik','calicut',9,99);
+insert into sdt  values (50001,'dude','kottayam','abcd@gmail.com'),
+(50003,'dominik','kannur','abcd@outlook.com'),(50005,'dominik','calicut','abce@gmail.com');
+
+use sakila;
+select * from film;
+
+create view film_view as
+select film_id,title,release_year, rental_rate,replacement_cost,rating from film;
+select * from film_view;
+
+select * from film_view where title like '%R';
+select * from film_view where rating='PG';
+select count(distinct title)from film where replacement_cost>20 and title like 'A%' ;
+select count(distinct title)from film where replacement_cost>20 or title like 'A%' ;
+select * from film_view where replacement_cost between 15 and 20;
